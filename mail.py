d33c0c08222402b62ef8f7c3d6bbe5e79b0e18cb
test <zengwj1949@sina.com>
2020-02-11 21:09:09 +0800

    0211

--- mail.py
+++ mail.py
@@ -1,34 +1,41 @@
-
-# 导入邮件相关模块；
+# 发送邮件模块；
 import smtplib
+# 构建发送字符串的邮件；
 from email.mime.text import MIMEText
+# 处理多种形态的邮件主体，如附件，是需要 MIMEMultipart 类；
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+# 构建图片相关的邮件，需要 MIMEImage 类；
+from email.mime.image import MIMEImage
 
-# 第三方SMTP发送邮件设置；
-smtpServer = "smtp.sina.com"
-sender = "zengwj1949@sina.com"
-passwd = "1949101Xl"
+def eMail():
+    # 第三方SMTP发送邮件设置；
+    smtpServer = "smtp.sina.com"
+    sender = "zengwj1949@sina.com"
+    passwd = "1949101Xl"
 
-"""
-发送相关信息
-"""
-# 设置发送内容，并把发送内容转换成邮件文本；
-message = "zwj is a chinese."
-msg = MIMEText(message)
-# 设置发送标题；
-msg["Subject"] = "来自帅哥的问候。"
-# 发送方相关信息，如发件人、SMTP服务器、端口、发件人登陆的用户及密码；
-msg["From"] = sender
+    # 邮件接收方地址；
+    toaddrs = ['2962372861@qq.com', 'zengwj1949@sina.com']
 
-try:
-    mailServer = smtplib.SMTP(smtpServer, 25)
-    mailServer.login(sender, passwd)
+    # 设置邮件信息；
+    # =============== 发送字符串邮件 ===============
+    # 设置发送邮件内容；
+    msg = MIMEText("CPU is Hight.")
+    # 设置邮件主题；
+    msg["Subject"] = "来自远方"
+    # 发送方信息，如发件人;
+    msg["From"] = sender
 
-    # 接收人邮件设置；
-    receivers = ["2962372861@qq.com", "zengwj1949@sina.com"]
+    try:
+        # 登陆邮件服务器，默认端口为25；
+        mailServer = smtplib.SMTP(smtpServer, 25)
+        mailServer.login(sender, passwd)
+        # 发送邮件;
+        mailServer.sendmail(sender, toaddrs, msg.as_string())
+        mailServer.quit()
+        print("Mail is Success.")
+    except Exception as e:
+        print("Error: ", e)
 
-    # 发送邮件，包含邮件发送方、邮件接收方、将MIMEText发送邮件的内容变成字符串等参数；
-    mailServer.sendmail(sender, receivers, msg.as_string())
-    mailServer.quit()
-    print("Email is Success.")
-except Exception as e:
-    print("Error: ", e)
+if __name__ == '__main__':
+    eMail()
